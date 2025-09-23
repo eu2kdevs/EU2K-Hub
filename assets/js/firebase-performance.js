ef9875f063010c7a00e0965085f2fe2ea16f7ff7
EU2K Devs <europa7e@gmail.com>
2025-09-23 07:12:33 +0200

    ömmm big comit

---
+++ assets/js/firebase-performance.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+    apiKey: "AIzaSyBRRVx6BtQtCDKjFYA8yh9qYrcUONmkkwI",
+    authDomain: "eu2k-hub.firebaseapp.com",
+    projectId: "eu2k-hub",
+    storageBucket: "eu2k-hub.firebasestorage.app",
+    messagingSenderId: "560244867055",
+    appId: "1:560244867055:web:3cd51b85baead94989001a",
+    measurementId: "G-2JDPR089WD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Performance Monitoring and get a reference to the service
+const perf = getPerformance(app);
