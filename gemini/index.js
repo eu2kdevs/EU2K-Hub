609e9a3afa2dbed509ea9439bb8b190f6b489f4b
EU2K Devs <europa7e@gmail.com>
2025-09-25 18:06:59 +0200

    Update index.js

--- gemini/index.js
+++ gemini/index.js
@@ -15,12 +15,11 @@ const geminiApiKey = defineSecret("GEMINI_API_KEY");
  * Cloud Function to retrieve Gemini API key via HTTP request
  * Supports CORS so it can be called from GitHub Pages
  */
-exports.getGeminiApiKey = onRequest(async (req, res) => {
+exports.getGeminiApiKey = onRequest({ region: "europe-west1" }, async (req, res) => {
   cors(req, res, async () => {
     try {
       logger.info("API key requested via HTTP");
 
-      // Return the API key
       res.json({
         apiKey: geminiApiKey.value(),
         success: true,
